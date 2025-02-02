5e27e8b6921eaf9d7846208d599a4e60e804506c
Burinot <jelourd.tayopan@gmail.com>
2025-02-03 00:58:26 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';   // Keep this for styles (if any)
+import LoginPage from './LoginPage'; // Import your LoginPage component
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />   {/* Add the LoginPage component here */}
+    </div>
+  );
+}
+
+export default App;

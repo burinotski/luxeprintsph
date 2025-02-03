e88e0abfb352443713fc9f249765f6f743d9adcb
Burinot <jelourd.tayopan@gmail.com>
2025-02-03 20:27:58 +0800

    Updated login.js with new changes on the code

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,41 +2,41 @@ import React from 'react';
 
 const LoginPage = () => {
   return (
-    <div data-layer="0.1. Log in" className="1LogIn" style={{width: 1920, height: 1080, paddingTop: 152, paddingBottom: 152, paddingLeft: 764, paddingRight: 702, background: '#363740', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
-    <div data-layer="log in" className="LogIn" style={{width: 454, height: 776, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
-      <div data-layer="cards / default" className="CardsDefault" style={{width: 454, height: 776, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
-        <div data-layer="sheet" className="Sheet" style={{width: 454, height: 776, background: 'white', borderRadius: 8, border: '1px #DFE0EB solid'}} />
+<div style={{width: 1920, height: 1080, paddingTop: 152, paddingBottom: 152, paddingLeft: 764, paddingRight: 702, background: '#363740', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+  <div style={{width: 454, height: 776, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+    <div style={{width: 454, height: 776, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+      <div style={{width: 454, height: 776, background: 'white', borderRadius: 8, border: '1px #DFE0EB solid'}} />
+    </div>
+    <img style={{width: 152, height: 46}} src="https://via.placeholder.com/152x46" />
+    <div style={{width: 394, height: 24, opacity: 0.70, textAlign: 'center', color: '#A4A6B3', fontSize: 19, fontFamily: 'Mulish', fontWeight: '700', letterSpacing: 0.40, wordWrap: 'break-word'}}>DIGITAL PRINTING SERVICES</div>
+    <div style={{width: 390, textAlign: 'center', color: '#252733', fontSize: 12, fontFamily: 'Mulish', fontWeight: '700', textTransform: 'uppercase', letterSpacing: 0.30, wordWrap: 'break-word'}}>Log in to your LuxePrints PH account</div>
+    <div style={{width: 390, textAlign: 'center', color: '#9FA2B4', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Enter your email and password below</div>
+    <div style={{width: 390, height: 64, position: 'relative'}}>
+      <div style={{width: 390, height: 42, left: 0, top: 22, position: 'absolute', background: '#FCFDFE', borderRadius: 8, border: '1px #F0F1F7 solid'}} />
+      <div style={{width: 366, left: 16, top: 33, position: 'absolute', opacity: 0.40, color: '#4B506D', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Email address</div>
+      <div style={{width: 390, height: 16, left: 0, top: 0, position: 'absolute', color: '#9FA2B4', fontSize: 12, fontFamily: 'Mulish', fontWeight: '700', textTransform: 'uppercase', letterSpacing: 0.30, wordWrap: 'break-word'}}>email</div>
+    </div>
+    <div style={{width: 390, height: 64, position: 'relative'}}>
+      <div style={{width: 390, height: 42, left: 0, top: 22, position: 'absolute', background: '#FCFDFE', borderRadius: 8, border: '1px #F0F1F7 solid'}} />
+      <div style={{width: 330, left: 16, top: 33, position: 'absolute', opacity: 0.40, color: '#4B506D', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Password</div>
+      <div style={{width: 20, height: 20, paddingLeft: 1.67, paddingRight: 1.67, paddingTop: 3.33, paddingBottom: 3.33, left: 354, top: 33, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 16.67, height: 13.33, background: '#9FA2B4'}}></div>
       </div>
-      <img data-layer="luxe_png_white_bg 1" className="LuxePngWhiteBg1" style={{width: 152, height: 46}} src="https://via.placeholder.com/152x46" />
-      <div data-layer="DIGITAL PRINTING SERVICES" className="DigitalPrintingServices" style={{width: 394, height: 24, opacity: 0.70, textAlign: 'center', color: '#A4A6B3', fontSize: 19, fontFamily: 'Mulish', fontWeight: '700', letterSpacing: 0.40, wordWrap: 'break-word'}}>DIGITAL PRINTING SERVICES</div>
-      <div data-layer="Log in to your LuxePrints PH account" className="LogInToYourLuxeprintsPhAccount" style={{width: 390, textAlign: 'center', color: '#252733', fontSize: 12, fontFamily: 'Mulish', fontWeight: '700', textTransform: 'uppercase', letterSpacing: 0.30, wordWrap: 'break-word'}}>Log in to your LuxePrints PH account</div>
-      <div data-layer="Enter your email and password below" className="EnterYourEmailAndPasswordBelow" style={{width: 390, textAlign: 'center', color: '#9FA2B4', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Enter your email and password below</div>
-      <div data-layer="textfield / label + textfield / default" className="TextfieldLabelTextfieldDefault" style={{width: 390, height: 64, position: 'relative'}}>
-        <div data-layer="textarea bg" className="TextareaBg" style={{width: 390, height: 42, left: 0, top: 22, position: 'absolute', background: '#FCFDFE', borderRadius: 8, border: '1px #F0F1F7 solid'}} />
-        <div data-layer="textfield" className="Textfield" style={{width: 366, left: 16, top: 33, position: 'absolute', opacity: 0.40, color: '#4B506D', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Email address</div>
-        <div data-layer="label" className="Label" style={{width: 390, height: 16, left: 0, top: 0, position: 'absolute', color: '#9FA2B4', fontSize: 12, fontFamily: 'Mulish', fontWeight: '700', textTransform: 'uppercase', letterSpacing: 0.30, wordWrap: 'break-word'}}>email</div>
-      </div>
-      <div data-layer="textfield / label + icon / default" className="TextfieldLabelIconDefault" style={{width: 390, height: 64, position: 'relative'}}>
-        <div data-layer="textarea bg" className="TextareaBg" style={{width: 390, height: 42, left: 0, top: 22, position: 'absolute', background: '#FCFDFE', borderRadius: 8, border: '1px #F0F1F7 solid'}} />
-        <div data-layer="textfield" className="Textfield" style={{width: 330, left: 16, top: 33, position: 'absolute', opacity: 0.40, color: '#4B506D', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Password</div>
-        <div data-layer="icon / hide / active" className="IconHideActive" style={{width: 20, height: 20, paddingLeft: 1.67, paddingRight: 1.67, paddingTop: 3.33, paddingBottom: 3.33, left: 354, top: 33, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
-          <div data-layer="Vector" className="Vector" style={{width: 16.67, height: 13.33, background: '#9FA2B4'}}></div>
-        </div>
-        <div data-layer="label" className="Label" style={{width: 390, height: 16, left: 0, top: 0, position: 'absolute', color: '#9FA2B4', fontSize: 12, fontFamily: 'Mulish', fontWeight: '700', textTransform: 'uppercase', letterSpacing: 0.30, wordWrap: 'break-word'}}>password</div>
-      </div>
-      <div data-layer="Forgot password?" className="ForgotPassword" style={{textAlign: 'right', color: '#9FA2B4', fontSize: 10, fontFamily: 'Mulish', fontWeight: '400', letterSpacing: 0.10, wordWrap: 'break-word'}}>Forgot password?</div>
-      <div data-layer="button / contained / primary / default" className="ButtonContainedPrimaryDefault" style={{width: 390, height: 48, position: 'relative'}}>
-        <div data-layer="btn bg" className="BtnBg" style={{width: 390, height: 48, left: 0, top: 0, position: 'absolute', background: '#9F09F0', boxShadow: '0px 4px 12px rgba(55, 81, 255, 0.24)', borderRadius: 8}} />
-        <div data-layer="btn label" className="BtnLabel" style={{width: 342, left: 24, top: 15, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Mulish', fontWeight: '600', lineHeight: 20, letterSpacing: 0.20, wordWrap: 'break-word'}}>Log In</div>
-      </div>
-      <div data-layer="account?" className="Account" style={{height: 20, position: 'relative'}}>
-        <div data-layer="Don’t have an account?" className="DonTHaveAnAccount" style={{left: 0, top: 0, position: 'absolute', textAlign: 'center', color: '#9FA2B4', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Don’t have an account?</div>
-        <div data-layer="button / text button / primary / default" className="ButtonTextButtonPrimaryDefault" style={{width: 51, height: 20, left: 163, top: 0, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
-          <div data-layer="btn label" className="BtnLabel" style={{width: 51, textAlign: 'center', color: '#3751FF', fontSize: 14, fontFamily: 'Mulish', fontWeight: '600', lineHeight: 20, letterSpacing: 0.20, wordWrap: 'break-word'}}>Sign up</div>
-        </div>
+      <div style={{width: 390, height: 16, left: 0, top: 0, position: 'absolute', color: '#9FA2B4', fontSize: 12, fontFamily: 'Mulish', fontWeight: '700', textTransform: 'uppercase', letterSpacing: 0.30, wordWrap: 'break-word'}}>password</div>
+    </div>
+    <div style={{textAlign: 'right', color: '#9FA2B4', fontSize: 10, fontFamily: 'Mulish', fontWeight: '400', letterSpacing: 0.10, wordWrap: 'break-word'}}>Forgot password?</div>
+    <div style={{width: 390, height: 48, position: 'relative'}}>
+      <div style={{width: 390, height: 48, left: 0, top: 0, position: 'absolute', background: '#9F09F0', boxShadow: '0px 4px 12px rgba(55, 81, 255, 0.24)', borderRadius: 8}} />
+      <div style={{width: 342, left: 24, top: 15, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Mulish', fontWeight: '600', lineHeight: 20, letterSpacing: 0.20, wordWrap: 'break-word'}}>Log In</div>
+    </div>
+    <div style={{height: 20, position: 'relative'}}>
+      <div style={{left: 0, top: 0, position: 'absolute', textAlign: 'center', color: '#9FA2B4', fontSize: 14, fontFamily: 'Mulish', fontWeight: '400', lineHeight: 20, letterSpacing: 0.30, wordWrap: 'break-word'}}>Don’t have an account?</div>
+      <div style={{width: 51, height: 20, left: 163, top: 0, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 51, textAlign: 'center', color: '#3751FF', fontSize: 14, fontFamily: 'Mulish', fontWeight: '600', lineHeight: 20, letterSpacing: 0.20, wordWrap: 'break-word'}}>Sign up</div>
       </div>
     </div>
   </div>
+</div>
   );
 };
 
